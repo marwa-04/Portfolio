2ec74a8018d7346dfad77072ca4596caa68a75f1
marwa-04 <marwaelazzazi@gmail.com>
2026-05-28 13:59:00 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 
 export default defineConfig({
+  base: '/Portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -14,3 +15,4 @@ export default defineConfig({
     open: true,
   },
 });
+
